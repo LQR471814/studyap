9db22e9b68a791bf7bc9c1664c386f18978b4ee2
seraph <106294171+seraph-yang@users.noreply.github.com>
2024-01-22 13:01:49 -0800

    Create page.tsx

---
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,10 @@
+import { SignUp } from '@clerk/nextjs';
+
+const SignUpPage = () => {
+  return (
+    <div className='mx-auto pt-20'>
+      <SignUp />
+    </div>
+  );
+};
+export default SignUpPage;
