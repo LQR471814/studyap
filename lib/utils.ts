25c4222d7dc017b50cd6693bad2298140d8df103
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2024-04-13 20:21:18 -0700

    remove nextjs

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,10 +1,3 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
-
 /**
  * Returns a list of segment lengths.
  *
